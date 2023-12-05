152a33ed1fa06d0de5be00088866dff771796b5c
Renier Vargas <renier1989@gmail.com>
2023-12-04 20:10:14 -0500

    436. Validando y Confirmando las cuentas

--- backend/controllers/usuarioController.ts
+++ backend/controllers/usuarioController.ts
@@ -9,14 +9,14 @@ interface ExpressReqRes {
 
 // funcion para registrar usuarios
 const registrarUsuario: ExpressReqRes = async (req, res) => {
-    // evitar registros con emails duplicados
-    const {email} = req.body;
-    const usuarioExiste = await Usuario.findOne({email});
+  // evitar registros con emails duplicados
+  const { email } = req.body;
+  const usuarioExiste = await Usuario.findOne({ email });
 
-    if(usuarioExiste){
-        const error = new Error('El usuario ya fue registrado, use otro email!');
-        return res.status(400).json({msg: error.message})
-    }
+  if (usuarioExiste) {
+    const error = new Error("El usuario ya fue registrado, use otro email!");
+    return res.status(400).json({ msg: error.message });
+  }
 
   try {
     const usuario = new Usuario(req.body);
@@ -29,57 +29,56 @@ const registrarUsuario: ExpressReqRes = async (req, res) => {
 };
 
 // funcion para autenticar a los usuarios
-const autenticar:ExpressReqRes = async(req,res)=>{
-    const {email,password} = req.body;
-    // 1. COMPORBAR SI EL USUARIO EXISTE 
-    const usuario = await Usuario.findOne({email});
-    if(!usuario){
-        const error = new Error(`El usuario no Existe!!!`);
-        return res.status(404).json({msg: error.message});
-    }  
-    // 2. COMPROBAR SI EL USUARIO ESTA CONFIRMADO
-    if(!usuario.confirmado){
-        const error = new Error(`Tu cuenta no ha sido confirmada.!!!`);
-        return res.status(404).json({msg: error.message});
-    }
-    // 3. COMPROBAR SU PASSWORD
-    if(await usuario.compararPassword(password)){
-        return res.status(200).json({
-            _id : usuario._id,
-            nombre: usuario.nombre,
-            email: usuario.email,
-            token : generarJWT(usuario.id)
-        });
-    }else{
-        const error = new Error(`El password es incorrecto.!!!`);
-        return res.status(404).json({msg: error.message});
-    }
-}
+const autenticar: ExpressReqRes = async (req, res) => {
+  const { email, password } = req.body;
+  // 1. COMPORBAR SI EL USUARIO EXISTE
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error(`El usuario no Existe!!!`);
+    return res.status(404).json({ msg: error.message });
+  }
+  // 2. COMPROBAR SI EL USUARIO ESTA CONFIRMADO
+  if (!usuario.confirmado) {
+    const error = new Error(`Tu cuenta no ha sido confirmada.!!!`);
+    return res.status(404).json({ msg: error.message });
+  }
+  // 3. COMPROBAR SU PASSWORD
+  if (await usuario.compararPassword(password)) {
+    return res.status(200).json({
+      _id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email,
+      token: generarJWT(usuario.id),
+    });
+  } else {
+    const error = new Error(`El password es incorrecto.!!!`);
+    return res.status(404).json({ msg: error.message });
+  }
+};
 
 // funcion para confirmar la cuenta de los usuarios con un token dinamico de sus cuentas
-const confirmar:ExpressReqRes = async(req,res)=>{
-const {token} = req.params
+const confirmar: ExpressReqRes = async (req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmar = await Usuario.findOne({ token });
+  if (!usuarioConfirmar) {
+    const error = new Error(`Token no valido.!!!`);
+    return res.status(404).json({ msg: error.message });
+  }
 
-console.log(token);
+  try {
+    usuarioConfirmar.confirmado= true;
+    usuarioConfirmar.token = '';
+    await usuarioConfirmar.save(); // se le puno un await porque esto consulta al servidor y espera para su ejecución
+    return res.status(200).json({msg: `Cuenta confirmada con exito.!!!`})
+  } catch (error) {
+    console.log(error);
+    
+  }
 
-
-}
-
+  
+};
 
 export { registrarUsuario, autenticar, confirmar };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // ejemplos de funciton que seran consumidas por las rutas
 // const usuario = (req:Request, res:Response) => {
