b521e1924a33026e97c0c3469a98b4134ab16785
Renier Vargas <renier1989@gmail.com>
2023-12-08 11:23:10 -0500

    453. Actualizar una Tarea

--- backend/controllers/tareaController.ts
+++ backend/controllers/tareaController.ts
@@ -3,6 +3,7 @@ import Tarea from "../models/Tarea";
 import Proyecto from "../models/Proyecto";
 import mongoose from "mongoose";
 import { isValidId } from "../helpers/validId";
+import { ITarea } from '../models/Tarea';
 
 interface ExpressReqRes {
   (req: Request | any, res: Response): void;
@@ -54,7 +55,36 @@ const obtenerTarea: ExpressReqRes = async (req, res) => {
     console.log(error);
   }
 };
-const actualizarTarea: ExpressReqRes = async (req, res) => {};
+const actualizarTarea: ExpressReqRes = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // esto es para que no salte un error si el id no es valido
+    if (!isValidId(id)) {
+      const error = new Error(`No pudimos encontrar la tarea.!!!`);
+      return res.status(404).json({ msg: error.message });
+    }
+    
+    const tarea:any = await Tarea.findById(id).populate("proyecto");
+
+    // verifico si puedo ver las tareas de un proyecto que no he creado
+    if (tarea?.proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error(
+        `No puedes editar esta Tarea.!!!`
+      );
+      return res.status(401).json({ msg: error.message });
+    }
+
+    tarea.nombre  = req.body.nombre || tarea.nombre;
+    tarea.descripcion  = req.body.descripcion || tarea.descripcion;
+    tarea.fechaEntrega  = req.body.fechaEntrega || tarea.fechaEntrega;
+    tarea.prioridad  = req.body.prioridad || tarea.prioridad;
+
+    const tareaActualizada = await tarea.save();
+    res.status(200).json(tareaActualizada);
+  } catch (error) {
+    console.log(error);
+  }
+};
 const eliminarTarea: ExpressReqRes = async (req, res) => {};
 const cambiarEstadoTarea: ExpressReqRes = async (req, res) => {};
 
