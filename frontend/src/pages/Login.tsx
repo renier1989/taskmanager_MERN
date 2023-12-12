26ac4924ee608d977d9591a6c3ba2111690d33c0
Renier Vargas <renier1989@gmail.com>
2023-12-11 20:53:09 -0500

    463. Creando la Página de Registrar

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,16 +13,16 @@ const Login = () => {
           <input id="email" autoComplete="off" type="email" placeholder="Email de usuario" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" autoFocus />
         </div>
         <div className="my-5">
-          <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="email">Password</label>
-          <input id="email" type="password" placeholder="Email de usuario" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" />
+          <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="password">Password</label>
+          <input id="password" type="password" placeholder="Tu password" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" />
         </div>
 
         <input type="submit" value="Iniciar Sesion" className="bg-sky-700 w-full py-3 text-white font-bold uppercase rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5" />
       </form>
 
       <nav className="lg:flex lg:justify-between ">
-        <Link to="registrar" className="block text-center my-5 uppercase text-slate-500 text-sm">¿No tienes cuenta? Registrarte</Link>
-        <Link to="olvide-password" className="block text-center my-5 uppercase text-slate-500 text-sm">¿Olvidaste tu Password? Registrarte</Link>
+        <Link to="/registrar" className="block text-center my-5 uppercase text-slate-500 text-sm">¿No tienes cuenta? Registrarte</Link>
+        <Link to="/olvide-password" className="block text-center my-5 uppercase text-slate-500 text-sm">¿Olvidaste tu Password?</Link>
       </nav>
     </>
 
--- frontend/src/pages/Registrar.tsx
+++ frontend/src/pages/Registrar.tsx
@@ -1,6 +1,37 @@
+import { Link } from "react-router-dom"
+
 const Registrar = () => {
   return (
-    <div>Registrar</div>
+    <>
+    <h1 className="text-sky-600 text-6xl font-black capitalize">
+      Crea tu cuentas y administra tus <span className="text-slate-700">{" "}proyectos</span>
+    </h1>
+    <form className="mt-20 bg-white rounded-lg shadow p-10">
+      <div className="my-5">
+        <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="nombre">Nombre</label>
+        <input id="nombre" autoComplete="off" type="text" placeholder="Tu nombre completo" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" autoFocus />
+      </div>
+      <div className="my-5">
+        <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="email">Email</label>
+        <input id="email" autoComplete="off" type="email" placeholder="Email de usuario" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" autoFocus />
+      </div>
+      <div className="my-5">
+        <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="password">Password</label>
+        <input id="password" type="password" placeholder="Tu password" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" />
+      </div>
+      <div className="my-5">
+        <label className="block uppercase text-gray-600 text-xl font-bold" htmlFor="password2">Repetir Password</label>
+        <input id="password2" type="password" placeholder="Repite tu password" className="w-full border p-3 mt-5 rounded-xl bg-gray-50" />
+      </div>
+
+      <input type="submit" value="Iniciar Sesion" className="bg-sky-700 w-full py-3 text-white font-bold uppercase rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5" />
+    </form>
+
+    <nav className="lg:flex lg:justify-between ">
+      <Link to="/" className="block text-center my-5 uppercase text-slate-500 text-sm">¿Ya tienes una cuenta? Inicia Sesion</Link>
+      <Link to="olvide-password" className="block text-center my-5 uppercase text-slate-500 text-sm">¿Olvidaste tu Password? Registrarte</Link>
+    </nav>
+  </>
   )
 }
 
