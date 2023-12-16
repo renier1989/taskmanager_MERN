cc108376b1790d3de65cc28ed7bc3c95b0767d9d
Renier Vargas <renier1989@gmail.com>
2023-12-15 22:57:13 -0500

    474. Confirmando la cuenta via Token

--- frontend/src/pages/ConfirmarCuenta.tsx
+++ frontend/src/pages/ConfirmarCuenta.tsx
@@ -1,9 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
+import Alerta from "../components/Alerta";
+
+interface AlertaData {
+  msg: string;
+  error: boolean;
+}
+
 const ConfirmarCuenta = () => {
+
+  const params = useParams()
+  const { id } = params;
+  const [alerta, setAlerta] = useState<AlertaData>({ msg: '', error: false })
+  const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
+
+
+  useEffect(() => {
+    const confirmarToken = async () => {
+      try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/api/usuarios/confirmar/${id}`;
+        const { data } = await axios(url);
+
+        setAlerta({
+          msg: data.msg,
+          error: false,
+        })
+
+        setCuentaConfirmada(true);
+
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true
+        })
+
+      }
+    }
+    return () => { confirmarToken() }
+
+  }, [id])
+
+  const { msg } = alerta;
+
   return (
     <>
-        <h1 className="text-sky-600 text-6xl font-black capitalize">
-      Confirma tu cuenta y empieza a crear tus <span className="text-slate-700">{" "}proyectos</span>
-    </h1>
+      <h1 className="text-sky-600 text-6xl font-black capitalize">
+        Confirma tu cuenta y empieza a crear tus <span className="text-slate-700">{" "}proyectos</span>
+      </h1>
+
+      <div className="mt-20 md:mt-10 shadow-lg bg-white rounded-xl px-5 py-10">
+        {msg && <Alerta alerta={alerta} />}
+
+        {cuentaConfirmada && (
+          <Link to="/" className="block text-center my-5 uppercase text-slate-500 text-sm"> Inicia Sesion</Link>
+        )}
+
+      </div>
     </>
   )
 }
