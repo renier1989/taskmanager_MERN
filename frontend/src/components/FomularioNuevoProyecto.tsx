08fa86cf9c6f689998998bad2ba18d7ce0515e32
Renier Vargas <renier1989@gmail.com>
2023-12-31 09:56:16 -0500

    493. Creando Proyectos

--- frontend/src/components/FomularioNuevoProyecto.tsx
+++ frontend/src/components/FomularioNuevoProyecto.tsx
@@ -7,47 +7,52 @@ export const FomularioNuevoProyecto = () => {
     const [fechaEntrega, setFechaEntrega] = useState<string>('')
     const [cliente, setCliente] = useState<string>('')
 
-    const {mostrarAlerta,alerta,registrarProyecto} = useProyecto()
+    const { mostrarAlerta, alerta, registrarProyecto } = useProyecto()
 
-    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        if([nombre,descripcion, fechaEntrega, cliente].includes('')){
+        if ([nombre, descripcion, fechaEntrega, cliente].includes('')) {
             mostrarAlerta({
                 msg: 'Todos los campos son obligatorias',
                 error: true
             })
-
             return
         }
 
         // aqui envio los datos del formulario para registrar el nuevo poryecto
-        registrarProyecto({nombre, descripcion, fechaEntrega,cliente})
+        await registrarProyecto({ nombre, descripcion, fechaEntrega, cliente })
+
+        // limpio el formulario
+        setNombre('');
+        setDescripcion('');
+        setCliente('');
+        setFechaEntrega('');
     }
 
-    const {msg} = alerta
+    const { msg } = alerta
 
-  return (
-    <form onSubmit={handleSubmit}  className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
-        {msg && <Alerta alerta={alerta} />}
-        <div className='mt-5'>
-            <label htmlFor="nombre" className="text-gray-700 uppercase font-bold text-sm">Nombre Proyecto</label>
-            <input value={nombre} onChange={e=>setNombre(e.target.value)} type="text" id="nombre" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Nombre del proyecto" />
-        </div>
-        <div className='mt-5'>
-            <label htmlFor="descripcion" className="text-gray-700 uppercase font-bold text-sm">Descripción</label>
-            <textarea value={descripcion} onChange={e=>setDescripcion(e.target.value)} id="descripcion" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Descripción del proyecto" />
-        </div>
-        <div className='mt-5'>
-            <label htmlFor="fecha-entrega" className="text-gray-700 uppercase font-bold text-sm">Fecha de Entrega</label>
-            <input value={fechaEntrega} onChange={e=>setFechaEntrega(e.target.value)} type="date" id="fecha-entrega" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" />
-        </div>
-        <div className='mt-5'>
-            <label htmlFor="cliente" className="text-gray-700 uppercase font-bold text-sm">Nombre Cliente</label>
-            <input value={cliente} onChange={e=>setCliente(e.target.value)} type="text" id="cliente" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Nombre del proyecto" />
-        </div>
+    return (
+        <form onSubmit={handleSubmit} className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
+            {msg && <Alerta alerta={alerta} />}
+            <div className='mt-5'>
+                <label htmlFor="nombre" className="text-gray-700 uppercase font-bold text-sm">Nombre Proyecto</label>
+                <input value={nombre} onChange={e => setNombre(e.target.value)} type="text" id="nombre" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Nombre del proyecto" />
+            </div>
+            <div className='mt-5'>
+                <label htmlFor="descripcion" className="text-gray-700 uppercase font-bold text-sm">Descripción</label>
+                <textarea value={descripcion} onChange={e => setDescripcion(e.target.value)} id="descripcion" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Descripción del proyecto" />
+            </div>
+            <div className='mt-5'>
+                <label htmlFor="fecha-entrega" className="text-gray-700 uppercase font-bold text-sm">Fecha de Entrega</label>
+                <input value={fechaEntrega} onChange={e => setFechaEntrega(e.target.value)} type="date" id="fecha-entrega" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" />
+            </div>
+            <div className='mt-5'>
+                <label htmlFor="cliente" className="text-gray-700 uppercase font-bold text-sm">Nombre Cliente</label>
+                <input value={cliente} onChange={e => setCliente(e.target.value)} type="text" id="cliente" className="border rounded-md w-full p-2 mt-2 placeholder-gray-400" placeholder="Nombre del proyecto" />
+            </div>
 
-        <input type="submit" value="Crear Proyecto" className='bg-sky-600 w-full uppercase rounded-md text-white font-bold p-3 cursor-pointer hover:shadow-lg transition duration-300 mt-5' />
-    </form>
-  )
+            <input type="submit" value="Crear Proyecto" className='bg-sky-600 w-full uppercase rounded-md text-white font-bold p-3 cursor-pointer hover:shadow-lg transition duration-300 mt-5' />
+        </form>
+    )
 }
--- frontend/src/context/ProyectosContext.tsx
+++ frontend/src/context/ProyectosContext.tsx
@@ -3,22 +3,51 @@ import { IProyectosContext, IProyectosProvider } from "../interfaces/IProyectos"
 import { IAlertData } from '../interfaces/IAlertData';
 import { IProyecto } from '../../../backend/models/Proyecto';
 import { TProyecto } from '../interfaces/ProyectoType';
+import AxiosClient from '../config/AxiosClient';
+import { useNavigate } from 'react-router-dom';
 
 const ProyectosContext = createContext<IProyectosContext>({} as IProyectosContext);
-const ProyectosProvider = ({children}:IProyectosProvider)=>{
+const ProyectosProvider = ({ children }: IProyectosProvider) => {
 
     const [proyectos, setProyectos] = useState('poryecto aqui')
     const [alerta, setAlerta] = useState<IAlertData>({} as IAlertData)
 
-    const mostrarAlerta = (alerta:IAlertData) =>{
+    const navigate = useNavigate()
+
+    const mostrarAlerta = (alerta: IAlertData) => {
+
         setAlerta(alerta);
         setTimeout(() => {
             setAlerta({} as IAlertData)
         }, 4000);
     }
 
-    const registrarProyecto = async (proyecto:TProyecto) =>{
-        console.log(proyecto);
+    const registrarProyecto = async (proyecto: TProyecto) => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) return
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            // registro el nuevo proyecto en la BD
+            await AxiosClient.post('/proyectos', proyecto, config);
+            // muestro una alerta del pryecto crado
+            setAlerta({
+                msg: 'Proyecto creado correctamente.!',
+                error: false
+            })
+            // aqui redirecciono a la vista de los proyectos
+            setTimeout(() => {
+                navigate('/proyectos')
+            }, 3000);
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
 
@@ -34,6 +63,6 @@ const ProyectosProvider = ({children}:IProyectosProvider)=>{
     )
 }
 
-export {ProyectosProvider}
+export { ProyectosProvider }
 
 export default ProyectosContext;
