1a4acf698e0c62c463f322c00316a332ce111c7d
Renier Vargas <renier1989@gmail.com>
2024-01-04 22:12:27 -0500

    515. Solucionar el Detalle con la Fecha

--- frontend/src/helpers/formatearFecha.tsx
+++ frontend/src/helpers/formatearFecha.tsx
@@ -1,5 +1,9 @@
 export const formatearFecha = (fecha:string) => {
-    const nuevaFecha = new Date(fecha)
+    const fechaN = fecha.split('T')[0].split('-');
+    const f = `${fechaN[1]}-${fechaN[2]}-${fechaN[0]}`;
+    // const nuevaFecha = new Date(fecha)
+    // const nuevaFecha = new Date(`${fechaN[2]}-${fechaN[1]}-${fechaN[0]}`)
+    const nuevaFecha = new Date(f)
 
     const options:Intl.DateTimeFormatOptions = {
         weekday: 'long',
