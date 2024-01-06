49c3493cea8c35bf5f25e5380e0f5b3884539f5b
Renier Vargas <renier1989@gmail.com>
2024-01-06 18:48:43 -0500

    539. Mostrar la Leyenda Colaborador cuando no son proyectos propios

--- frontend/src/components/PreviewProyecto.tsx
+++ frontend/src/components/PreviewProyecto.tsx
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom';
 
 import { IFProyecto } from '../interfaces/IProyectos';
+import useAuth from '../hooks/useAuth';
 interface props {
     proyecto: IFProyecto
 }
 
 export const PreviewProyecto = ({ proyecto }: props) => {
-    // console.log(proyecto);
-    const { _id, cliente, nombre } = proyecto
+    const { auth } = useAuth()
+    const { _id, cliente, nombre, creador } = proyecto
     return (
-        <div className='border-b p-5 flex'>
-            <p className='flex-1'>{nombre}
-                <span className='text-sm text-gray-500 uppercase'>
-                    {' '}{cliente}
-                </span>
-            </p>
+        <div className='border-b p-5 flex justify-between'>
+            <div className='flex items-center gap-2'>
+                <p className='flex-1 '>{nombre}
+                    <span className='text-sm text-gray-500 uppercase'>
+                        {' '}{cliente}
+                    </span>
+                </p>
+                {auth._id !== creador &&
+                    <p className='px-2 py-1 bg-green-500 text-white ml-3 text-sm font-semibold rounded-md'>Colaborador</p>
+                }
+            </div>
             <Link className='text-gray-600 hover:text-gray-800 uppercase text-sm font-bold' to={`${_id}`}> Ver Proyecto </Link>
         </div>
     )
