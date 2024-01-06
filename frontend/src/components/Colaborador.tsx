fdb77fa19ec21089ea24ddaa50be986ba21321d1
Renier Vargas <renier1989@gmail.com>
2024-01-06 10:16:43 -0500

    529. Mostrando la Información de Colaboradores

--- frontend/src/components/Colaborador.tsx
+++ frontend/src/components/Colaborador.tsx
@@ -1,11 +1,22 @@
 import { TColaborador } from "../interfaces/ColaboradorType"
 
 interface props {
-    colaborador : TColaborador
+    colaborador: TColaborador
 }
 
-export const Colaborador = ({colaborador}:props) => {
-  return (
-    <div>Colaborador</div>
-  )
+export const Colaborador = ({ colaborador }: props) => {
+    const { nombre, email } = colaborador
+    return (
+        <div className="flex items-center justify-between py-5 px-5">
+            <div>
+                <p className="font-bold text-xl">{nombre}</p>
+                <p className="text-sm font-semibold text-gray-700">{email}</p>
+            </div>
+            <div>
+                <button className="py-2 px-4 text-white font-bold bg-red-600 text-sm uppercase rounded-lg hover:shadow-lg transition-shadow duration-300">
+                    Eliminar
+                </button>
+            </div>
+        </div>
+    )
 }
